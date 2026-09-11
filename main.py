1e11fc8023f6020a5c0e47b8b14279aee444d172
Tipulate <isaacarrubla@gmail.com>
2026-09-11 10:30:10 -0500

    feat: Made "reiniciar_votacion" export votos aswell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 usuarios = dict()
+votos = {"Team Green": 0,"Team Black": 0,}
 
 def registrar_voto():
     pass
@@ -12,6 +13,11 @@ def reiniciar_votacion():
         "usuarios.csv", index=False
     )
 
+    pd.DataFrame(list(votos.items()), columns=["team", "votos"]).to_csv(
+        "votos.csv", index=False
+    )
 
+
+reiniciar_votacion()
 while True:
     pass
